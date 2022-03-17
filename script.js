0b1207e5104dc336f3599bfd57c985cd2306226e
Leoanardo santos <leosantosmelo07@gmail.com>
2022-03-17 17:21:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let seconds= 0;
+let minutes= 0;
+let hours= 0;
+let countdown;
+
+function start(){
+    countdown= setInterval(()=>{
+        timer();
+    },1000)
+}
+
+function pause(){
+   clearInterval(countdown);
+}
+
+function stop(){
+   clearInterval(countdown);
+   seconds= 0;
+   minutes= 0;
+   hours= 0;
+   document.getElementById("countdown-area").innerHTML= `00:00:00`
+}
+function timer(){
+    seconds ++;
+    if(seconds==60){
+        seconds= 0;
+        minutes ++;
+    }
+    if(minutes == 60){
+        minutes= 0;
+        hours++;
+    }
+
+    let format= 
+    (hours <10 ? '0'+hours:hours)+
+    ":"+
+    (minutes <10 ? '0'+minutes:minutes)+
+    ":"+
+    (seconds <10 ? '0'+seconds:seconds)
+
+    document.getElementById("countdown-area").innerHTML= format;
+
+}
